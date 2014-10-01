4635b91be161a19d04ec2289631e4d778b106b2b
InquisitorCrow <nashalrath@hotmail.co.uk>
2014-10-01 23:39:05 +0100

    Finished work (ish)
    Work was completed, but strangely wont work (but isn't displaying any
    errors)

--- GP2Labs-RMCCRO201/main.cpp
+++ GP2Labs-RMCCRO201/main.cpp
@@ -34,6 +34,41 @@ const int WINDOW_HEIGHT = 480;
 bool running = true;
 
 // Global functions
+
+// Function to draw
+void render()
+{
+	// Set the clear colour (background)
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// clear the colour and depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Switch to ModelView
+	glMatrixMode(GL_MODELVIEW);
+	//Reset using the Indentity Matrix
+	glLoadIdentity();
+	//Translate to -5.0f on z-axis
+	glTranslatef(0.0f, 0.0f, -5.0f);
+	//Begin drawing triangles
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f); //Colour of the vertices
+	glVertex3f(0.0f, 1.0f, 0.0f); // Top
+	glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+	glVertex3f(1.0f, -1.0f, 0.0f); // Bottom Right
+	glEnd();
+
+
+	// require to swap back and front buffer
+	SDL_GL_SwapWindow(window);
+
+
+}
+// Function to update the game state
+void update()
+{
+
+}
 void CleanUp(){
 	SDL_GL_DeleteContext(glcontext);
 	
@@ -124,6 +159,9 @@ int main(int argc, char * arg[]) {
 				running = false;
 			}
 		}
+
+		update();
+		render();
 	}
 
 
